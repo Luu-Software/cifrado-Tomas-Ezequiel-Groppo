f920d5d0444965cd9cd89c3eeaa38989f4f38bcb
GROPPO TOMAS EZEQUIEL <50976178@ort>
2026-06-02 15:50:24 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import { preguntar } from './lib/consola.ts';
 import { cifrar, descifrar } from './lib/funcionesCriptográficas.ts';
 
-let palabra: string = ''; // Preguntar por la palabra a cifrar o descifrar
-let claveTexto: string = ''; // Preguntar por la clave de cifrado (número)
-let accion: string = ''; // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
+let palabra: string = preguntar('¿Que palabra desea cifrar/descifrar? '); // Preguntar por la palabra a cifrar o descifrar
+let claveTexto: string = preguntar('¿Cual es la clave de cifrado? '); // Preguntar por la clave de cifrado (número)
+let accion: string = preguntar('¿Desea cifrar o descifrar la palabra? '); // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
 
-let resultado: string = ''; // COMPLETAR
-console.log(resultado);
+let resultado: string = '';
+
+if (accion==='cifrar' || accion==='Cifrar'){
+
+resultado = cifrar(palabra, Number(claveTexto));
+console.log('La palabra es' + resultado);
+
+}
+else if (accion==='descifrar' || accion==='Descifrar'){
+
+resultado=descifrar(palabra, Number(claveTexto));
+
+console.log('La palabra es: ' + resultado);
+
+}
+else{
+
+    console.log('El código no funciona como vos querés. Reintenta poniendo un valor válido')
+
+}
+
+
