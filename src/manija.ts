3e94a65747d55f54543eb1199924d2f1650aaba6
GROPPO TOMAS EZEQUIEL <50976178@ort>
2026-06-23 15:44:38 -0300

    Manija terminada

--- src/manija.ts
+++ src/manija.ts
@@ -1,17 +1,34 @@
 import { preguntar } from './lib/consola.ts';
-let tamañoPrefijo = 
+let tamañoPrefijo : string = preguntar("Cuantos digitos queres analizar")
 function obtenerPrefijo(numero: number, tamañoPrefijo: number): number {
-  return numero / 10 ^ 16-(numero, tamañoPrefijo);
+  return Math.floor(numero / 10 ** (16- tamañoPrefijo));
 }
 
-let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
+let tarjetaTexto: string = preguntar('Ingresa el numero de tarjeta: ');
 
-let red: string = ''; // COMPLETAR
-let prefijo;
-prefijo=obtenerPrefijo(Number(tarjetaTexto), 2)
 
-if (prefijo===4){
-red=''
+let red: string = "No es un numero valido"
+
+if (Number(tamañoPrefijo)===2){
+  if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===34 ||(Number(tarjetaTexto), Number(tamañoPrefijo)) === 37){
+    red="Amex"
+  }
+  else if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===51 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 52 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 53 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 54 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 55){
+    red="Mastercard"
+  }
+  else{
+    red = "No es un numero valido"
+  }
 }
+else if (Number(tamañoPrefijo)===1){
+  if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===4){
+    red="VISA"
+  }
+  else{
+    "No es un numero valido"
+  }
+}
+
+
 console.log(red);
 //Tiene error, algún día lo soluciono :)
