e07d8caf611c4116cff85b1b7aabb9e5713199ce
GROPPO TOMAS EZEQUIEL <50976178@ort>
2026-06-23 15:51:54 -0300

    Idea para la consigna manija

--- src/manija.ts
+++ src/manija.ts
@@ -16,11 +16,19 @@ if (Number(tamañoPrefijo)===2){
   else if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===51 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 52 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 53 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 54 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 55){
     red="Mastercard"
   }
+  else if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===40 || (Number(tarjetaTexto), Number(tamañoPrefijo))===41 || (Number(tarjetaTexto), Number(tamañoPrefijo))===42 || (Number(tarjetaTexto), Number(tamañoPrefijo))===43 || (Number(tarjetaTexto), Number(tamañoPrefijo))===44 || (Number(tarjetaTexto), Number(tamañoPrefijo))===45 || (Number(tarjetaTexto), Number(tamañoPrefijo))===46 || (Number(tarjetaTexto), Number(tamañoPrefijo))===47 || (Number(tarjetaTexto), Number(tamañoPrefijo))===48 || (Number(tarjetaTexto), Number(tamañoPrefijo))===49){
+    red="VISA"
+  }
   else{
     red = "No es un numero valido"
   }
+  if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===40 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 41 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 42 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 43 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 44 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 45 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 46 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 47 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 48 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 49){
+    console.log("Hola")
+    console.log(obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo)))
+  }
 }
 else if (Number(tamañoPrefijo)===1){
+  
   if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===4){
     red="VISA"
   }
@@ -28,7 +36,6 @@ else if (Number(tamañoPrefijo)===1){
     "No es un numero valido"
   }
 }
-
 
 console.log(red);
 //Tiene error, algún día lo soluciono :)
