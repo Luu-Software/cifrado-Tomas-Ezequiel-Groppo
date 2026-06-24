aa9d9538f1f31586089fd7c07c0af7ab5c553753
GROPPO TOMAS EZEQUIEL <50976178@ort>
2026-06-24 08:24:07 -0300

    Manija versión personal

--- src/manija.ts
+++ src/manija.ts
@@ -1,5 +1,5 @@
 import { preguntar } from './lib/consola.ts';
-let tamañoPrefijo : string = preguntar("Cuantos digitos queres analizar")
+let tamañoPrefijo : number = 2;
 function obtenerPrefijo(numero: number, tamañoPrefijo: number): number {
   return Math.floor(numero / 10 ** (16- tamañoPrefijo));
 }
@@ -10,21 +10,17 @@ let tarjetaTexto: string = preguntar('Ingresa el numero de tarjeta: ');
 let red: string = "No es un numero valido"
 
 if (Number(tamañoPrefijo)===2){
-  if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===34 ||(Number(tarjetaTexto), Number(tamañoPrefijo)) === 37){
+  if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===34 ||obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo)) === 37){
     red="Amex"
   }
-  else if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===51 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 52 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 53 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 54 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 55){
+  else if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===51 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo)) === 52 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo)) === 53 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo)) === 54 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo)) === 55){
     red="Mastercard"
   }
-  else if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===40 || (Number(tarjetaTexto), Number(tamañoPrefijo))===41 || (Number(tarjetaTexto), Number(tamañoPrefijo))===42 || (Number(tarjetaTexto), Number(tamañoPrefijo))===43 || (Number(tarjetaTexto), Number(tamañoPrefijo))===44 || (Number(tarjetaTexto), Number(tamañoPrefijo))===45 || (Number(tarjetaTexto), Number(tamañoPrefijo))===46 || (Number(tarjetaTexto), Number(tamañoPrefijo))===47 || (Number(tarjetaTexto), Number(tamañoPrefijo))===48 || (Number(tarjetaTexto), Number(tamañoPrefijo))===49){
+  else if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===40 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo))===41 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo))===42 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo))===43 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo))===44 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo))===45 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo))===46 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo))===47 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo))===48 || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo))===49){
     red="VISA"
   }
   else{
     red = "No es un numero valido"
-  }
-  if (obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo))===40 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 41 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 42 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 43 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 44 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 45 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 46 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 47 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 48 || (Number(tarjetaTexto), Number(tamañoPrefijo)) === 49){
-    console.log("Hola")
-    console.log(obtenerPrefijo (Number(tarjetaTexto), Number(tamañoPrefijo)))
   }
 }
 else if (Number(tamañoPrefijo)===1){
